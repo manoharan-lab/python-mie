49bab79c4e6748a93736815667017fa964e73c1b
AnnieStephenson <stephenson@g.harvard.edu>
2019-03-25 15:21:04 -0400

    Added functions to make compatible with polarization calculations (#4)
    * added functions and tests to keep track of polarization in lab frame
    
    * updated mie calculations for absolute polarization and updated tests
    
    * combined functions for cartesian and scattering plane coordinate systems into single functions
    
    * vectorized calculations in theta
    
    * finished vectorizing most functions and updated tests
    
    * vectorizeall functions in theta and removed functions that take scalar theta only
    
    * addressed minor code changes requested in review
    
    * added more explanation to docstring for amplitude_scattering_matrix()

--- pymie/tests/test_mie.py
+++ pymie/tests/test_mie.py
@@ -24,6 +24,7 @@ from .. import Quantity, ureg, q, index_ratio, size_parameter, np, mie
 from nose.tools import assert_raises, assert_equal
 from numpy.testing import assert_almost_equal, assert_array_almost_equal
 from pint.errors import DimensionalityError
+import pytest
 
 def test_cross_sections():
     # Test cross sections against values calculated from BHMIE code (originally
@@ -91,7 +92,7 @@ def test_form_factor():
     ipar, iperp = mie.calc_ang_dist(m, x, angles)
     assert_array_almost_equal(ipar, ipar_bhmie)
     assert_array_almost_equal(iperp, iperp_bhmie)
-
+    
 def test_efficiencies():
     x = np.array([0.01, 0.01778279, 0.03162278, 0.05623413, 0.1, 0.17782794,
                   0.31622777, 0.56234133, 1, 1.77827941, 3.16227766, 5.62341325,
@@ -399,10 +400,55 @@ def test_cross_section_Sudiarta():
     assert_almost_equal(cabs3.to('um^2').magnitude, cabs4.to('um^2').magnitude, decimal=6)
     assert_almost_equal(cext3.to('um^2').magnitude, cext4.to('um^2').magnitude, decimal=6)
 
+def test_pis_taus():
+    '''
+    Checks that the vectorized pis_and_taus matches the scalar pis_and_taus
+    '''
+    
+    # number of terms to keep
+    nstop = 3
+    
+    # check that result for a vector input matches the result for a scalar input
+    theta = np.pi/4
+    pis, taus = mie._pis_and_taus(nstop,theta)
+    pis_v, taus_v = mie._pis_and_taus(nstop, np.array(theta))
+    
+    assert_almost_equal(pis, pis_v)
+    assert_almost_equal(taus, taus_v)
+
+   # check that result for a vector input matches the result for a scalar input
+   # for a theta 1d array
+    theta = np.array([np.pi/4, np.pi/2, np.pi/3])
+    pis_v, taus_v = mie._pis_and_taus(nstop, theta)
+    pis = np.zeros((len(theta), nstop))
+    taus = np.zeros((len(theta), nstop))
+    for i in range(len(theta)):
+        pis[i,:], taus[i,:] = mie._pis_and_taus(nstop, theta[i])
+        
+    assert_almost_equal(pis, pis_v)
+    assert_almost_equal(taus, taus_v)
+    
+    # check that result for a vector input matches the result for a scalar input
+    # for a theta 2d array
+    theta = np.array([[np.pi/4, np.pi/2, np.pi/3],[np.pi/6, np.pi/4, np.pi/2]])
+    pis_v, taus_v = mie._pis_and_taus(nstop, theta)
+    pis = np.zeros((theta.shape[0], theta.shape[1], nstop))
+    taus = np.zeros((theta.shape[0], theta.shape[1], nstop))
+    for i in range(theta.shape[0]):
+        for j in range(theta.shape[1]):
+            pis[i,j,:], taus[i,j,:] = mie._pis_and_taus(nstop, theta[i,j])
+    
+    assert_almost_equal(pis, pis_v)
+    assert_almost_equal(taus, taus_v)
+
+
 def test_cross_section_complex_medium():
+    
     # test that the cross sections calculated with the exact Mie solutions 
     # match the far-field Mie solutions and Sudiarta and Fu's solutions when 
     # there is no absorption in the medium
+    
+    # set parameters
     wavelen = Quantity('400 nm')
     n_particle = Quantity(1.5+0.01j,'') 
     n_matrix = Quantity(1.0,'') 
@@ -477,3 +523,191 @@ def test_cross_section_complex_medium():
 
     assert_almost_equal(cscat_exact2.to('um^2').magnitude, cscat_sudiarta2.to('um^2').magnitude, decimal=4)
     assert_almost_equal(cscat_exact2.to('um^2').magnitude, cscat_fu2.to('um^2').magnitude, decimal=4)
+    
+    
+def test_vector_scattering_amplitude_2d_theta_cartesian():
+    '''
+    Test that the amplitude scattering vector assuming x-polarized incident
+    light calculated by amp_scat_vec_2d_theta_xy() matches what we get by doing
+    the matrix multiplication manually
+    
+    amp_scat_vec_2d_theta_xy() converts from the parallel/perpendicular basis
+    by doing the change of basis matrix multiplication in the function
+    
+    Here, we carry out the change of basis manually and plug in the numbers
+    to make sure the two methods match.
+    
+    [as_vec_x]  = [cos(phi)  sin(phi)] * [S2  0] * [cos(phi)  sin(phi)] * [1]
+    [as_vec_y]    [sin(phi) -cos(phi)]   [0  S1]   [sin(phi) -cos(phi)]   [0]
+                        
+                = [S2cos(phi)^2       +       S1sin(phi)^2]
+                  [S2cos(phi)sin(phi) - S1cos(phi)sin(phi)]
+    
+    '''
+    
+    # parameters of sample and source
+    wavelen = Quantity('658 nm')
+    radius = Quantity('0.85 um')
+    n_matrix = Quantity(1.00, '')
+    #n_particle = Quantity(1.59 + 1e-4 * 1.0j, '')
+    n_particle = Quantity(1.59, '')
+    thetas = Quantity(np.linspace(np.pi/2, np.pi, 2), 'rad')
+    phis = Quantity(np.linspace(0, 2*np.pi, 4), 'rad')
+    thetas_2d, phis_2d = np.meshgrid(thetas, phis) 
+    
+    # parameters for calculating scattering
+    m = index_ratio(n_particle, n_matrix)
+    x = size_parameter(wavelen, n_matrix, radius)
+    
+    # calculate the amplitude scattering matrix in xy basis
+    as_vec_x0, as_vec_y0 = mie.vector_scattering_amplitude(m, x, thetas_2d, 
+                            coordinate_system = 'cartesian', phis = phis_2d)
+    
+    # calcualte the amplitude scattering matrix in par/perp basis
+    S1_sp, S2_sp, S3_sp, S4_sp = mie.amplitude_scattering_matrix(m, x, thetas_2d)
+    
+    as_vec_x = S2_sp*np.cos(phis_2d)**2 + S1_sp*np.sin(phis_2d)**2
+    as_vec_y = S2_sp*np.cos(phis_2d)*np.sin(phis_2d) - S1_sp*np.cos(phis_2d)*np.sin(phis_2d)
+    
+    assert_almost_equal(as_vec_x0, as_vec_x)
+    assert_almost_equal(as_vec_y0, as_vec_y)
+    
+    
+def test_diff_scat_intensity_complex_medium_cartesian():
+    '''
+    Test that the magnitude of the differential scattered intensity is the 
+    same in the xy basis as it is in the parallel, perpendicular basis
+    
+    This should be true because a rotation around phi brings the par/perp basis
+    into the x,y basis
+    '''
+    # parameters of sample and source
+    wavelen = Quantity('658 nm')
+    radius = Quantity('0.85 um')
+    n_matrix = Quantity(1.00 + 1e-4* 1.0j, '')
+    n_particle = Quantity(1.59 + 1e-4 * 1.0j, '')
+    thetas = Quantity(np.linspace(np.pi/2, np.pi, 4), 'rad')
+    phis = Quantity(np.linspace(0, 2*np.pi, 3), 'rad')
+    thetas_2d, phis_2d = np.meshgrid(thetas, phis)
+    thetas_2d = Quantity(thetas_2d, 'rad')
+    
+    # parameters for calculating scattering
+    m = index_ratio(n_particle, n_matrix)
+    x = size_parameter(wavelen, n_matrix, radius)
+    kd = 2*np.pi*n_matrix/wavelen*Quantity(10000,'nm')
+    
+    # calculate differential scattered intensity in xy basis
+    I_x, I_y = mie.diff_scat_intensity_complex_medium(m, x, thetas_2d, kd, 
+                            coordinate_system = 'cartesian', phis = phis_2d)
+    
+    # calcualte differential scattered intensity in par/perp basis
+    I_par, I_perp = mie.diff_scat_intensity_complex_medium(m, x, thetas_2d, kd)
+    
+    # assert equality of their magnitudes
+    I_xy_mag = np.sqrt(I_x**2 + I_y**2)
+    I_par_perp_mag = np.sqrt(I_par**2 + I_perp**2)
+    
+    # check that the magnitudes are equal
+    assert_almost_equal(I_xy_mag, I_par_perp_mag)
+   
+def test_integrate_intensity_complex_medium_cartesian():
+    '''
+    Test that when integrated over all theta and phi angles, the intensities
+    calculated in the par/perp basis match those calculated in the x/y basis
+    '''
+    # parameters of sample and source
+    wavelen = Quantity('658 nm')
+    radius = Quantity('0.85 um')
+    n_matrix = Quantity(1.00 + 1e-4* 1.0j, '')
+    n_particle = Quantity(1.59 + 1e-4 * 1.0j, '')
+    thetas = Quantity(np.linspace(np.pi/2, np.pi, 20), 'rad')
+    phis = Quantity(np.linspace(0, 2*np.pi, 20), 'rad')
+    thetas_2d, phis_2d = np.meshgrid(thetas, phis)
+    thetas_2d = Quantity(thetas_2d, 'rad')
+    
+    # parameters for calculating scattering
+    m = index_ratio(n_particle, n_matrix)
+    x = size_parameter(wavelen, n_matrix, radius)
+    k = 2*np.pi*n_matrix/wavelen
+    distance = Quantity(10000,'nm')
+    kd = k*distance
+    
+    # calculate the differenetial scattered intensities
+    I_x, I_y = mie.diff_scat_intensity_complex_medium(m, x, thetas_2d, kd,
+                            coordinate_system = 'cartesian', phis = phis_2d)
+    
+    I_par, I_perp = mie.diff_scat_intensity_complex_medium(m, x, thetas, kd)
+
+    
+    # integrate the differential scattered intensities
+    cscat_xy = mie.integrate_intensity_complex_medium(I_x, I_y, distance, thetas, k,
+                         coordinate_system = 'cartesian', phis = phis)[0]
+    
+    cscat_parperp = mie.integrate_intensity_complex_medium(I_par, I_perp, 
+                                                         distance, thetas, k)[0]
+    
+    # check that the integrated cross sections are equal
+    assert_almost_equal(cscat_xy.magnitude, cscat_parperp.magnitude)
+    
+def test_value_errors():
+    '''
+    test the errors related to incorrect input
+    '''
+    
+    # parameters of sample and source
+    wavelen = Quantity('658 nm')
+    radius = Quantity('0.85 um')
+    n_matrix = Quantity(1.00 + 1e-4* 1.0j, '')
+    n_particle = Quantity(1.59 + 1e-4 * 1.0j, '')
+    thetas = Quantity(np.linspace(np.pi/2, np.pi, 4), 'rad')
+    phis = Quantity(np.linspace(0, 2*np.pi, 3), 'rad')
+    thetas_2d, phis_2d = np.meshgrid(thetas, phis)
+    thetas_2d = Quantity(thetas_2d, 'rad')
+    
+    # parameters for calculating scattering
+    m = index_ratio(n_particle, n_matrix)
+    x = size_parameter(wavelen, n_matrix, radius)
+    k = 2*np.pi*n_matrix/wavelen
+    distance = Quantity(10000,'nm')
+    kd = k*distance
+    
+    with pytest.raises(ValueError):
+        # try to calculate differential scattered intensity in weird coordinate system
+        I_x, I_y = mie.diff_scat_intensity_complex_medium(m, x, thetas_2d, kd, 
+                            coordinate_system = 'weird', phis = phis_2d)
+    
+    # calculate the differenetial scattered intensities
+    I_x, I_y = mie.diff_scat_intensity_complex_medium(m, x, thetas_2d, kd,
+                            coordinate_system = 'cartesian', phis = phis_2d)
+    
+    I_par, I_perp = mie.diff_scat_intensity_complex_medium(m, x, thetas, kd)
+    
+    with pytest.raises(ValueError):
+        # integrate the differential scattered intensities
+        cscat_xy = mie.integrate_intensity_complex_medium(I_x, I_y, distance, 
+                        thetas, k, coordinate_system = 'cartesian')[0]
+        
+        cscat_weird = mie.integrate_intensity_complex_medium(I_x, I_y, distance, 
+                        thetas, k, coordinate_system = 'weird')[0]
+        
+        as_vec_weird = mie.vector_scattering_amplitude(m, x, thetas_2d, 
+                            coordinate_system = 'weird', phis = phis_2d)
+        
+        as_vec_xy = mie.vector_scattering_amplitude(m, x, thetas_2d, 
+                            coordinate_system = 'cartesian')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
